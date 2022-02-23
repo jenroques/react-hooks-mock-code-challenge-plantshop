f7330a062e8b05723e09f33fd0c84b3667ad9cac
Jennifer <jenniferroques@gmail.com>
2022-02-23 15:22:04 -0600

    Completed

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
+import PlantCard from "./PlantCard";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
-function PlantPage() {
+function PlantPage({ }) {
+  const [plants, setPlants] = useState([]); 
+  const [filterPlants, setFilterPlants] = useState(plants)
+
+  function handleSearch(e) {
+    const filteredPlants = plants.filter(plant => {
+      return plant.name.includes(e.target.value)
+    })
+
+    setFilterPlants(filterPlants)
+  }
+
+  function addNewPlant(newPlant) {
+
+    const updatedPlants = [...plants, newPlant]
+    setPlants(updatedPlants)
+  }
+
+  useEffect(() => {
+    setFilterPlants(plants)
+  }, [plants])
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then(resp => resp.json())
+    .then(data => {
+      setPlants(data)})
+    }, [])
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm addNewPlant={addNewPlant}/>
+      <Search handleSearch={handleSearch}/>
+      <PlantList plants={filterPlants}/>
     </main>
   );
 }
